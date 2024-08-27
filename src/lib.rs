8449675096b0a3721223b600e99268c7a7dd3654
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-27 21:58:56 +0200

    skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,16 @@
-use nvim_oxi::api::{self, opts::*, types::*, Window};
-use nvim_oxi::{print, Dictionary, Function};
+use nvim_oxi::{api, print, Dictionary, Function};
 
 #[nvim_oxi::plugin]
 fn vity() -> nvim_oxi::Result<Dictionary> {
-    let load: Function<(), Result<(), api::Error>> = Function::from_fn(move |()| {
-        print!("vity loaded"); // debug purpose
+	let load: Function<(), Result<(), api::Error>> = Function::from_fn(move |()| {
+		print!("vity loaded"); // debug purpose
 
-        Ok(())
-    });
+		Ok(())
+	});
 
-    // avoid `load.clone()` and gives possible plugin extension
-    let setup: &Function<(), Result<(), api::Error>> = &load;
-
-    // avoid `load.clone()`
-    let colorscheme: &Function<(), Result<(), api::Error>> = &load;
-
-    Ok(Dictionary::from_iter([
-        ("load", load),
-        ("setup", setup),
-        ("colorscheme", colorscheme),
-    ]))
+	Ok(Dictionary::from_iter([
+		("load", load.clone()),
+		("setup", load.clone()),
+		("colorscheme", load),
+	]))
 }
