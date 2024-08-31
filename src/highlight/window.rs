e52a9acfb383f014b33c7b55facbf48f997d31de
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-31 22:43:06 +0200

    Pmenu

--- src/highlight/window.rs
+++ src/highlight/window.rs
@@ -1,11 +1,20 @@
 use nvim_oxi::api::{self, opts::SetHighlightOpts};
 
+use crate::{color::BACKGROUND, hsl};
+
 pub fn load() -> Result<(), api::Error> {
 	api::set_hl(
 		0,
 		"Normal",
 		&SetHighlightOpts::builder()
-			.background(&crate::color::BACKGROUND.to_rgb())
+			.background(&BACKGROUND.to_rgb())
+			.build(),
+	)?;
+	api::set_hl(
+		0,
+		"Pmenu",
+		&SetHighlightOpts::builder()
+			.background(&hsl(BACKGROUND.h, BACKGROUND.s, BACKGROUND.l - 4).to_rgb())
 			.build(),
 	)?;
 
