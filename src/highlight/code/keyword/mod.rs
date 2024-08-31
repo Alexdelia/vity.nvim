c5c5a8efb560d0c9604dea1df2239f85767c9afe
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-31 20:06:02 +0200

    keyword

--- src/highlight/code/keyword/mod.rs
+++ src/highlight/code/keyword/mod.rs
@@ -1,8 +1,18 @@
 mod flow;
 
-use nvim_oxi::api;
+use nvim_oxi::api::{self, opts::SetHighlightOpts};
+
+use crate::hsl;
 
 pub fn load() -> Result<(), api::Error> {
+	api::set_hl(
+		0,
+		"Keyword",
+		&SetHighlightOpts::builder()
+			.foreground(&hsl(54, 66, 46))
+			.build(),
+	)?;
+
 	flow::load()?;
 
 	Ok(())
