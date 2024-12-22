c3a2afc9fb5d80938f516bda45d483e8afe706e7
Alexdelia <alexandre.delille.57@gmail.com>
2024-12-22 13:04:52 +0100

    correct color

--- src/highlight/code/namespace.rs
+++ src/highlight/code/namespace.rs
@@ -7,15 +7,14 @@ pub fn load() -> Result<(), api::Error> {
 		0,
 		"@module",
 		&SetHighlightOpts::builder()
-			.foreground(&hsl(60, 53, 55))
-			.blend(2 / 3)
+			.foreground(&hsl(60, 40, 40))
 			.build(),
 	)?;
 
 	api::set_hl(
 		0,
 		"@lsp.type.namespace",
-		&SetHighlightOpts::builder().link("Identifier").build(),
+		&SetHighlightOpts::builder().link("@module").build(),
 	)?;
 
 	Ok(())
