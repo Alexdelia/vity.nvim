21c2acac34fb59e83987581a6d86e5ff66249f69
Alexdelia <alexandre.delille.57@gmail.com>
2024-12-22 12:34:30 +0100

    upgrade

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ pkgs.rustPlatform.buildRustPackage {
     lockFile = ./Cargo.lock;
 
     outputHashes = {
-      "nvim-oxi-0.5.1" = "sha256-SHAXUZIxAEtAt40xFmOJeyRy6+jeXOcU8O7tC6pCvY4=";
+      "nvim-oxi-0.5.1" = "sha256-pyEOWNhhJ5sjAjq8KG5tMWIhs1U8J0P2TknLpk9QbLk=";
     };
   };
 
