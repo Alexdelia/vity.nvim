b87d6867ee4f439927a8c5e513e93afefe063b16
Alexdelia <alexandre.delille.57@gmail.com>
2024-12-25 23:04:05 +0100

    more keyword type support

--- src/highlight/code/keyword/flow.rs
+++ src/highlight/code/keyword/flow.rs
@@ -3,11 +3,34 @@ use crate::hsl;
 use nvim_oxi::api::{self, opts::SetHighlightOpts};
 
 pub fn load() -> Result<(), api::Error> {
+	let base = Hsl {
+		h: 45,
+		s: 81,
+		l: 55,
+	};
 	api::set_hl(
 		0,
 		"Conditional",
 		&SetHighlightOpts::builder()
-			.foreground(&hsl(45, 81, 55))
+			.foreground(&base.to_rgb())
+			.build(),
+	)?;
+	let f = follow!("Conditional");
+	api::set_hl(0, "@keyword.conditional", &f)?;
+
+	api::set_hl(
+		0,
+		"@keyword.return",
+		&SetHighlightOpts::builder()
+			.foreground(&hsl(base.h, base.s - 5, base.l - 5))
+			.build(),
+	)?;
+
+	api::set_hl(
+		0,
+		"@keyword.operator",
+		&SetHighlightOpts::builder()
+			.foreground(&hsl(base.h + 17, base.s, base.l))
 			.build(),
 	)?;
 
