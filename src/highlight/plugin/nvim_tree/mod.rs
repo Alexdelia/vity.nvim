5b8e4b321b9513db156018942a98ac36063afd93
Alexdelia <alexandre.delille.57@gmail.com>
2026-04-14 17:22:27 +0200

    feat: better nvim tree support

--- src/highlight/plugin/nvim_tree/mod.rs
+++ src/highlight/plugin/nvim_tree/mod.rs
@@ -1,11 +1,24 @@
 mod git;
 
-use nvim_oxi::api;
+use nvim_oxi::api::{self, opts::SetHighlightOpts};
+
+use const_format::formatcp as f;
+
+use crate::hsl;
 
 const PRE: &str = "NvimTree";
 
 pub fn load() -> Result<(), api::Error> {
 	git::load()?;
+
+	api::set_hl(
+		0,
+		f!("{PRE}FolderIcon"),
+		&SetHighlightOpts::builder()
+			.foreground(&hsl(201, 15, 62))
+			.bold(true)
+			.build(),
+	)?;
 
 	Ok(())
 }
