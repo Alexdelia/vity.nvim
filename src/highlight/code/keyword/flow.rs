2fca7d176ecd45dc416f4d43a2c51be5698a277d
Alexdelia <alexandre.delille.57@gmail.com>
2026-06-14 15:35:00 +0200

    feat: additional conditional key

--- src/highlight/code/keyword/flow.rs
+++ src/highlight/code/keyword/flow.rs
@@ -19,6 +19,7 @@ pub fn load() -> Result<(), api::Error> {
 	api::set_hl(0, "@keyword.conditional", f)?;
 	api::set_hl(0, "@lsp.mod.controlFlow", f)?;
 	api::set_hl(0, "@lsp.typemod.operator.controlFlow", f)?;
+	api::set_hl(0, "@lsp.typemod.keyword.conditional", f)?;
 	api::set_hl(0, "@lsp.typemod.keyword.control", f)?;
 	api::set_hl(0, "rustQuestionMark", f)?;
 
